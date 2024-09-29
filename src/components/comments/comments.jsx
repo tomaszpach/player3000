daedb426eb9c2ad56805f21b6f32848a65f6a11e
tomaszglazer <tomaszglazer@icloud.com>
2024-09-29 13:52:18 +0200

    init

--- src/components/comments/comments.jsx
+++ src/components/comments/comments.jsx
@@ -1,52 +1,18 @@
 import React from 'react';
 import './comments.scss';
+import { CommentsRender } from './commentsRender';
 
 export const Comments = ({ comments }) => {
-    console.log('ttt comments', comments);
+    const commentsNumber = comments?.length;
+    const firstTenComments = comments?.slice(0, 10);
 
-    const timeAgo = (date) => {
-        const now = new Date();
-        const past = new Date(date);
-        const seconds = Math.floor((now - past) / 1000);
-        const intervals = {
-            year: 31536000,
-            month: 2592000,
-            week: 604800,
-            day: 86400,
-            hour: 3600,
-            minute: 60,
-            second: 1,
-        };
+    if (commentsNumber === 0) {
+        return <h1>No comments yet</h1>;
+    }
 
-        for (const [unit, value] of Object.entries(intervals)) {
-            const count = Math.floor(seconds / value);
-            if (count >= 1) {
-                return count === 1 ? `a ${unit} ago` : `${count} ${unit}s ago`;
-            }
-        }
+    if (commentsNumber > 10) {
+        return CommentsRender({ comments: firstTenComments });
+    }
 
-        return 'just now';
-    };
-    return (
-        <div className="comments">
-            <h1>Comments</h1>
-            {comments?.map((comment) => (
-                <div className="single-comment" key={comment.id}>
-                    <div className="author-dateAdded">
-                        <h4 className="author">@{comment.author}</h4>
-
-                        <p
-                            className="dateAdded"
-                            title={'Comment added: ' + comment.dateAdded}
-                        >
-                            {timeAgo(comment.dateAdded)}
-                        </p>
-                        <p className="likes">{comment.likes} likes</p>
-                    </div>
-
-                    <p className="comment">{comment.comment}</p>
-                </div>
-            ))}
-        </div>
-    );
+    return CommentsRender({ comments });
 };
---
+++ src/components/comments/commentsRender.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './comments.scss';
+import { timeAgo } from '../../helpers/dates';
+
+export const CommentsRender = ({ comments }) => {
+    return (
+        <div className="comments">
+            <h1>Comments</h1>
+            {comments?.map((comment) => (
+                <div className="single-comment" key={comment.id}>
+                    <div className="author-dateAdded">
+                        <h4 className="author">@{comment.author}</h4>
+
+                        <p
+                            className="dateAdded"
+                            title={'Comment added: ' + comment.dateAdded}
+                        >
+                            {timeAgo(comment.dateAdded)}
+                        </p>
+                        <p className="likes">{comment.likes} likes</p>
+                    </div>
+
+                    <p className="comment">{comment.comment}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
