434513039281474a0fa7a6a989a993ea31d27fc1
tomaszglazer <tomaszglazer@icloud.com>
2024-09-29 12:29:42 +0200

    init

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,13 +3,15 @@ import { Logo } from './logo';
 import './header.scss';
 import { Title } from './title';
 import { Link } from 'react-router-dom';
+import { Logo2 } from './logo2';
 
 export const Header = () => {
     return (
         <header id="header" className="section">
             <div className="logo-title">
                 <Link to="/" className="logo-link">
-                    <Logo />
+                    {/*<Logo />*/}
+                    <Logo2 />
                 </Link>
                 <Title />
             </div>
